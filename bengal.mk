84aaef9e4405a5df6ec005d4f6bdf88cc7edd4a2
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-12 23:47:56 +0200

    chime: Enabled updateable APEX
    Change-Id: Iae46cbf48ae5fbfa100e83e8d6ce19f225102cf3
    Signed-off-by: hac4us06 to <hac4usanime@gmail.com>

--- bengal.mk
+++ bengal.mk
@@ -1,9 +1,11 @@
-
 #
 # Copyright (C) 2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)

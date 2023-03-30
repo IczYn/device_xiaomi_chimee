b4c93502431df65e48bc97b9a0f95eecb27d8b82
roynatech2544 <whiteshell2544@naver.com>
2023-03-30 18:09:37 +0900

    chime: Test

--- chime.mk
+++ chime.mk
@@ -106,7 +106,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -117,7 +118,8 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libstdc++.vendor \
-    libgui_vendor
+    libgui_vendor \
+    libpiex_shim
 
 # Component overrides
 PRODUCT_COPY_FILES += \
@@ -157,7 +159,11 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libsdmcore \
     libsdmutils \
-    libtinyxml
+    libtinyxml \
+    libtinyxml.vendor \
+    libqservice.vendor \
+    vendor.qti.hardware.servicetracker@1.2.vendor \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libvulkan
